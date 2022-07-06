68ba31eea57b7b363894e26d256ba4c51c2c8827
Maxwell6612 <Maxwell6612@protonmail.ch>
2022-07-06 22:28:40 +0300

    add mapbox

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upload.wikimedia.org", "links.papareact.com", "images.unsplash.com"], 
+    domains: ["upload.wikimedia.org", 
+              "links.papareact.com", 
+              "images.unsplash.com"], 
   },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoibWF4d2VsbDY2MTIiLCJhIjoiY2w1OGh0Z3NuMDBhNjNxano3amgyYnJnaSJ9.O3FF3ytTVEiZ9o7jYecUdA'
+  }
 }
